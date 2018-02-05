44ba9deb7d54bedc51f7ad2cd3be35d1674fe996
Griffin P Longtin and Mikey Pinaud <mikey@>
2018-02-05 10:16:09 -0800

    initialize directories, installed packages and dependencies, create page layout and setup JSX, React and Babel

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import TicketList from "./TicketList";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <TicketList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "./Ticket"
+
+function TicketList() {
+  return (
+    <Ticket
+      location="2B"
+      names="Mikey and Griffin"
+      issue="React not working in Atom!"/>
+  );
+}
+
+export default TicketList;
