8c6a57a96d01be0fb1a54bd0ab5dae28198480f3
Mikey Pinaud and Sailor Winkelman <sailor@>
2018-02-13 10:17:23 -0800

    put application state in app component and add created inverse data flow between NewTicketForm and Ticket components using PropTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,40 +4,57 @@ import NewTicketControl from './NewTicketControl';
 import TicketList from './TicketList';
 import { Switch, Route } from 'react-router-dom';
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div>
-      <style jsx global>{`
-        body {
-          font-family: Helvetica;
-        }
-        .box {
-          border: none;
-          display: block;
-          border-bottom: 2px solid #fff;
-          margin-bottom: 10px;
-        }
-        .box:hover {
-          border-bottom: 2px solid #ccc;
-          outline: 0;
-        }
-        a {
-          color: #888;
-          text-decoration: none;
-        }
-      `}</style>
-      <div style={{margin: '19px auto 0', width: 800}}>
-        <div className="box">
-          <Header />
-          <Switch>
-            <Route exact path='/' component={TicketList} />
-            <Route path='/newticket' component={NewTicketControl} />
-          </Switch>
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  handleAddingNewTicketToList(newTicket){
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render() {
+    return (
+      <div>
+        <style jsx global>{`
+            body {
+              font-family: Helvetica;
+            }
+            .box {
+              border: none;
+              display: block;
+              border-bottom: 2px solid #fff;
+              margin-bottom: 10px;
+            }
+            .box:hover {
+              border-bottom: 2px solid #ccc;
+              outline: 0;
+            }
+            a {
+              color: #888;
+              text-decoration: none;
+            }
+        `}</style>
+        <div style={{margin: '19px auto 0', width: 800}}>
+          <div className="box">
+            <Header />
+            <Switch>
+              <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+              <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 
 export default App;
--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
 import NewTicketForm from './NewTicketForm';
+import PropTypes from 'prop-types';
 
 class NewTicketControl extends React.Component {
 
@@ -16,10 +17,11 @@ class NewTicketControl extends React.Component {
     this.setState({formVisibleOnPage: true});
   }
 
+
   render(){
     let currentlyVisibleContent = null;
     if (this.state.formVisibleOnPage){
-      currentlyVisibleContent = <NewTicketForm />;
+      currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation}/>;
     } else {
       currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
     }
@@ -30,5 +32,9 @@ class NewTicketControl extends React.Component {
     );
   }
 }
+
+NewTicketControl.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketControl;
