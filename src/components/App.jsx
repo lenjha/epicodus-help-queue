247e8fa9cd9c89daf239249038836d33820dd787
Guest User <Guest@Epicodus-1C.local>
2018-02-14 10:47:57 -0800

    create event handler for ticket time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+// import Moment from 'moment';
 import NewTicketControl from './NewTicketControl';
 import TicketList from './TicketList';
 import { Switch, Route } from 'react-router-dom';
@@ -15,8 +16,28 @@ class App extends React.Component {
   }
 
   handleAddingNewTicketToList(newTicket){
-    var newMasterTicketList = this.state.masterTicketList.slice();
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
     newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+    60000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime() {
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) =>
+      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+    );
     this.setState({masterTicketList: newMasterTicketList});
   }
 
